3ab7eb06215021ef4d833af77b8f53e07a7f14bd
alchain <alchaincyf@gmail.com>
2024-09-23 23:09:47 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 
 import localFont from "next/font/local";
 import "./globals.css";
+import { useEffect } from 'react';
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,10 +21,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-M0EW39RN13');
+  }, []);
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M0EW39RN13"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-M0EW39RN13');
+          `}
+        </Script>
       </body>
     </html>
   );
